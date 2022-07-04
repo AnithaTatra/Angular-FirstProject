1ceabb19ae6de6ddec16f81a3a3a37da876ee881
AnithaTatra <102857170+AnithaTatra@users.noreply.github.com>
2022-07-04 09:04:05 +0530

    Committed

--- src/app/signin-login/signin-login.component.ts
+++ src/app/signin-login/signin-login.component.ts
@@ -5,11 +5,58 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './signin-login.component.html',
   styleUrls: ['./signin-login.component.css']
 })
-export class SigninLoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class SigninLoginComponent  {
+   List:any;
+  
+  isVisible: boolean = true;
+  username:string='';
+  password:string='';
+  mobileNumber:string='';
+  email:string='';
+  ClickHere() {
+    
+    
+    console.log('UserName:',this.username);
+    console.log('PassWord:',this.password);
+    console.log('MobileNumber:',this.mobileNumber);
+    console.log('Email:',this.email);
+   
+    
+     this.List = [
+      
+     
+      {
+        username:this.username,
+       
+      },
+      {
+        password:this.password,
+        
+      },
+      {
+        mobileNumber: this.mobileNumber,
+       
+      },
+      {
+        email: this.email,
+       
+      },
+      
+    ]
+    console.log('UserName:',this.List);
   }
 
-}
+   
+    
+    hideList(){
+      this.isVisible = false;
+  }
+  
+  
+  }
+  
+  
+  
+  
+
+
